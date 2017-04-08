55cf3f5a99b77bc946affac376b717ce880ba2c5
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-04-09 00:15:45 +0530

    Auto push

--- cronjob.sh
+++ cronjob.sh
@@ -2,5 +2,9 @@ export DISPLAY=:0.0
 if (( kill $(ps aux | grep '[x]fce4-terminal -e' | awk '{print $2}') )); then
 	python3 /home/hunter/workspace/dctrends/move_logs.py
 	rm /home/hunter/.ncdc/stderr.log
+	cd /home/hunter/workspace/dctrends
+	git add -A
+	git commit -m "Update"
+	git push origin master
 	/usr/bin/xfce4-terminal -e "/home/hunter/workspace/ncdc" &
 fi
