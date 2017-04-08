0f7fb932a80dc3b34e03c0fd3cf0ed7f7a49b611
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-04-09 00:13:06 +0530

    Fix cron bug

--- cronjob.sh
+++ cronjob.sh
@@ -1,6 +1,6 @@
+export DISPLAY=:0.0
 if (( kill $(ps aux | grep '[x]fce4-terminal -e' | awk '{print $2}') )); then
 	python3 /home/hunter/workspace/dctrends/move_logs.py
 	rm /home/hunter/.ncdc/stderr.log
+	/usr/bin/xfce4-terminal -e "/home/hunter/workspace/ncdc" &
 fi
-
-xfce4-terminal -e "/home/hunter/workspace/ncdc" &
