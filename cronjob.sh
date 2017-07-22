972a52e6c2c5c5441b22746d2f5f97a340a9651c
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-07-22 07:07:57 +0530

    Update

--- cronjob.sh
+++ cronjob.sh
@@ -3,9 +3,11 @@ if (( kill $(ps aux | grep '[x]fce4-terminal -e' | awk '{print $2}') )); then
 	python3 /home/hunter/workspace/dctrends/move_logs.py
 	rm /home/hunter/.ncdc/stderr.log
 	cd /home/hunter/workspace/dctrends
-        git pull origin master
+    git pull origin master
 	git add -A
 	git commit -m "Update"
 	git push origin master
 	/usr/bin/xfce4-terminal -e "/home/hunter/workspace/ncdc" &
+    kill $(ps aux | grep '[b]rahma' | awk '{print $2}')
+    python3 /home/hunter/workspace/dcaudio/brahma.py
 fi
